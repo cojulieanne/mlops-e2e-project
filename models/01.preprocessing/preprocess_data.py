a031fc2bc85e315be5d678c4866cae08718e16fd
Cutiepatotieee <ced.encarnacion@gmail.com>
2025-07-16 05:22:17 +0800

    added black and flake8 to pre-commit config. 4 files auto edited by black

--- models/01.preprocessing/preprocess_data.py
+++ models/01.preprocessing/preprocess_data.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 
-df = pd.read_csv('data/bronze/ml2_student_performance.csv')
-df['Pass/Fail (1=Pass, 0=Fail)'] = df['Pass/Fail (1=Pass, 0=Fail)'].apply(lambda x: 1 if x == 0 else 0)
-df.to_csv('data/silver/preprocessed_ml2_student_performance.csv', index=False)
+df = pd.read_csv("data/bronze/ml2_student_performance.csv")
+df["Pass/Fail (1=Pass, 0=Fail)"] = df["Pass/Fail (1=Pass, 0=Fail)"].apply(
+    lambda x: 1 if x == 0 else 0
+)
+df.to_csv("data/silver/preprocessed_ml2_student_performance.csv", index=False)
--- models/03.training/training.py
+++ models/03.training/training.py
@@ -8,36 +8,33 @@ from tqdm import tqdm
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import (RandomForestClassifier,
-                              GradientBoostingClassifier)
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.model_selection import train_test_split, KFold, StratifiedKFold
 from sklearn.metrics import accuracy_score
 
 import warnings
+
 warnings.filterwarnings("ignore")
 
 # metrics
 from imblearn.metrics import sensitivity_score, geometric_mean_score
 
-#resampling methods
+# resampling methods
 from imblearn.over_sampling import RandomOverSampler, SMOTE, ADASYN
 from imblearn.under_sampling import RandomUnderSampler
 
 # pipeline
 from imblearn.pipeline import Pipeline
 
-df = pd.read_csv('data/silver/preprocessed_ml2_student_performance.csv')
+df = pd.read_csv("data/silver/preprocessed_ml2_student_performance.csv")
 
-X, y = df.drop('Pass/Fail (1=Pass, 0=Fail)', axis=1), df['Pass/Fail (1=Pass, 0=Fail)']
+X, y = df.drop("Pass/Fail (1=Pass, 0=Fail)", axis=1), df["Pass/Fail (1=Pass, 0=Fail)"]
 
-(X_trainval, X_holdout, y_trainval, y_holdout) = train_test_split(X, y,
-                                                                  random_state=143,
-                                                                  test_size=0.25,
-                                                                  stratify=y)
+(X_trainval, X_holdout, y_trainval, y_holdout) = train_test_split(
+    X, y, random_state=143, test_size=0.25, stratify=y
+)
 
-models_dict = {
-    'RandomForestClassifier': RandomForestClassifier(random_state=143)
-}
+models_dict = {"RandomForestClassifier": RandomForestClassifier(random_state=143)}
 
 skf = StratifiedKFold(n_splits=5)
 
@@ -55,29 +52,29 @@ for model_name, model in tqdm(models_dict.items()):
         X_train, X_val = X_trainval.iloc[train_index], X_trainval.iloc[val_index]
         y_train, y_val = y_trainval.iloc[train_index], y_trainval.iloc[val_index]
 
-        start_time = time.time() # for logging run times
+        start_time = time.time()  # for logging run times
 
         # fit
         model.fit(X_train, y_train)
 
-        #predict
+        # predict
         val_preds = model.predict(X_val)
 
         val_rec_score = sensitivity_score(y_val, val_preds)
         val_gmean_score = geometric_mean_score(y_val, val_preds)
         val_accuracy_score = accuracy_score(y_val, val_preds)
 
-        end_time = time.time() # for logging run times
+        end_time = time.time()  # for logging run times
 
         val_rec_scores.append(val_rec_score)
         val_gmean_scores.append(val_gmean_score)
         val_accuracy_scores.append(val_accuracy_score)
 
     res[model_name] = {
-        'ave_val_recall':np.mean(val_rec_scores) * 100,
-        'ave_val_gmean_score':np.mean(val_gmean_scores) * 100,
-        'ave_val_accuracy_score':np.mean(val_accuracy_scores) * 100,
-        'run_time': end_time - start_time
+        "ave_val_recall": np.mean(val_rec_scores) * 100,
+        "ave_val_gmean_score": np.mean(val_gmean_scores) * 100,
+        "ave_val_accuracy_score": np.mean(val_accuracy_scores) * 100,
+        "run_time": end_time - start_time,
     }
 
 # log end time
