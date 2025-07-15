d8e1ed5c3533d4b0b727a71a6bb5e525d174f77a
Cutiepatotieee <ced.encarnacion@gmail.com>
2025-07-16 05:13:45 +0800

    added pre-commit config and fixed the issues identified by it

--- load_data.py
+++ load_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 file_id = '16_IoRl6EUCevWf4_l5orzRLjlnVG-WUd'
 url = f'https://drive.google.com/uc?id={file_id}'
-output = 'data/bronze/ml2_student_performance.csv'  
+output = 'data/bronze/ml2_student_performance.csv'
 
 gdown.download(url, output, quiet=False)
 
