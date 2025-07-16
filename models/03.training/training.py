bdcd69c9ed750c6a9f5fb33c9b14fc47960764dc
Cutiepatotieee <ced.encarnacion@gmail.com>
2025-07-16 13:19:38 +0800

    Modified training.py to include additional classifiers

--- models/03.training/training.py
+++ models/03.training/training.py
@@ -26,19 +26,19 @@ from imblearn.under_sampling import RandomUnderSampler
 # pipeline
 from imblearn.pipeline import Pipeline
 
-df = pd.read_csv("data/silver/preprocessed_ml2_student_performance.csv")
+df = pd.read_csv("data/gold/train_ml2_students_data.csv")
 
-X, y = df.drop("Pass/Fail (1=Pass, 0=Fail)", axis=1), df["Pass/Fail (1=Pass, 0=Fail)"]
+X_trainval, y_trainval = df.drop("Pass/Fail (1=Pass, 0=Fail)", axis=1), df["Pass/Fail (1=Pass, 0=Fail)"]
 
-(X_trainval, X_holdout, y_trainval, y_holdout) = train_test_split(
-    X, y, random_state=143, test_size=0.25, stratify=y
-)
-
-models_dict = {"RandomForestClassifier": RandomForestClassifier(random_state=143)}
+models_dict = {
+    'LogisticRegressor': LogisticRegression(penalty='l2'),
+    'DecisionTreeClassifier': DecisionTreeClassifier(random_state=143),
+    'RandomForestClassifier': RandomForestClassifier(random_state=143)
+}
 
 skf = StratifiedKFold(n_splits=5)
 
-res = {}
+undersampler = {}
 
 # log start time
 total_start = time.time()
@@ -52,29 +52,31 @@ for model_name, model in tqdm(models_dict.items()):
         X_train, X_val = X_trainval.iloc[train_index], X_trainval.iloc[val_index]
         y_train, y_val = y_trainval.iloc[train_index], y_trainval.iloc[val_index]
 
-        start_time = time.time()  # for logging run times
+        start_time = time.time() # for logging run times
 
-        # fit
-        model.fit(X_train, y_train)
+        pipeline = Pipeline([('RandomUnderSampler',
+                              RandomUnderSampler(random_state=143)),
+                             (model_name, model)])
+        pipeline.fit(X_train, y_train)
 
-        # predict
-        val_preds = model.predict(X_val)
+        train_preds = pipeline.predict(X_train)
+        val_preds = pipeline.predict(X_val)
 
         val_rec_score = sensitivity_score(y_val, val_preds)
         val_gmean_score = geometric_mean_score(y_val, val_preds)
         val_accuracy_score = accuracy_score(y_val, val_preds)
-
-        end_time = time.time()  # for logging run times
+        
+        end_time = time.time() # for logging run times
 
         val_rec_scores.append(val_rec_score)
         val_gmean_scores.append(val_gmean_score)
         val_accuracy_scores.append(val_accuracy_score)
 
-    res[model_name] = {
-        "ave_val_recall": np.mean(val_rec_scores) * 100,
-        "ave_val_gmean_score": np.mean(val_gmean_scores) * 100,
-        "ave_val_accuracy_score": np.mean(val_accuracy_scores) * 100,
-        "run_time": end_time - start_time,
+    undersampler[model_name] = {
+        'ave_val_recall':np.mean(val_rec_scores) * 100,
+        'ave_val_gmean_score':np.mean(val_gmean_scores) * 100,
+        'ave_val_accuracy_score':np.mean(val_accuracy_scores) * 100,
+        'run_time': end_time - start_time
     }
 
 # log end time
@@ -82,5 +84,5 @@ total_end = time.time()
 
 elapsed = total_end - total_start
 # print(f"Report Generated in {elapsed:.2f} seconds")
-res = pd.DataFrame(res).T
-print(res)
+undersampler = pd.DataFrame(undersampler).T
+print(undersampler)
