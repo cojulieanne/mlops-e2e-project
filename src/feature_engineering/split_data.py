66885348025b3094a2ecce9783df4ecfc2f14343
Cutiepatotieee <ced.encarnacion@gmail.com>
2025-07-16 16:42:32 +0800

    minor adjustments on split_data.py for pipeline compatibility

--- src/feature_engineering/split_data.py
+++ src/feature_engineering/split_data.py
@@ -1,20 +1,21 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split, KFold, StratifiedKFold
 
-df = pd.read_csv("data/silver/preprocessed_ml2_student_performance.csv")
+def split_data(df):
+    df = pd.read_csv("data/silver/preprocessed_ml2_student_performance.csv")
 
-X, y = df.drop("Pass/Fail (1=Pass, 0=Fail)", axis=1), df["Pass/Fail (1=Pass, 0=Fail)"]
+    X, y = df.drop("Pass/Fail (1=Pass, 0=Fail)", axis=1), df["Pass/Fail (1=Pass, 0=Fail)"]
 
-(X_trainval, X_holdout, y_trainval, y_holdout) = train_test_split(
-    X, y, random_state=143, test_size=0.25, stratify=y
-)
+    (X_trainval, X_holdout, y_trainval, y_holdout) = train_test_split(
+        X, y, random_state=143, test_size=0.25, stratify=y
+    )
 
-train_df = X_trainval.copy()
-train_df["Pass/Fail (1=Pass, 0=Fail)"] = y_trainval
+    train_df = X_trainval.copy()
+    train_df["Pass/Fail (1=Pass, 0=Fail)"] = y_trainval
 
-test_df = X_holdout.copy()
-test_df["Pass/Fail (1=Pass, 0=Fail)"] = y_holdout
+    test_df = X_holdout.copy()
+    test_df["Pass/Fail (1=Pass, 0=Fail)"] = y_holdout
 
 
-train_df.to_csv("data/gold/train_ml2_students_data.csv", index=False)
-test_df.to_csv("data/gold/test_ml2_students_data.csv", index=False)
+    train_df.to_csv("data/gold/train_ml2_students_data.csv", index=False)
+    test_df.to_csv("data/gold/test_ml2_students_data.csv", index=False)
