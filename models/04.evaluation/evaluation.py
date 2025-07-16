3592d6214deeeb9c1b7546326ebddc1f90bc18c2
Cutiepatotieee <ced.encarnacion@gmail.com>
2025-07-16 13:53:27 +0800

    Finalized evaluation.py and tested

--- models/04.evaluation/evaluation.py
+++ models/04.evaluation/evaluation.py
@@ -26,61 +26,24 @@ from imblearn.under_sampling import RandomUnderSampler
 # pipeline
 from imblearn.pipeline import Pipeline
 
-df = pd.read_csv("data/silver/preprocessed_ml2_student_performance.csv")
+test_df = pd.read_csv("data/gold/test_ml2_students_data.csv")
+train_df = pd.read_csv("data/gold/train_ml2_students_data.csv")
 
-X, y = df.drop("Pass/Fail (1=Pass, 0=Fail)", axis=1), df["Pass/Fail (1=Pass, 0=Fail)"]
 
-(X_trainval, X_holdout, y_trainval, y_holdout) = train_test_split(
-    X, y, random_state=143, test_size=0.25, stratify=y
-)
+model = RandomForestClassifier(random_state=143)
 
-models_dict = {"RandomForestClassifier": RandomForestClassifier(random_state=143)}
+pipeline = Pipeline([('RandomUnderSampler',
+                              RandomUnderSampler(random_state=143)),
+                             ('RFClassifier',model)])
 
-skf = StratifiedKFold(n_splits=5)
 
-res = {}
+X_train, y_train = train_df.drop("Pass/Fail (1=Pass, 0=Fail)", axis=1), train_df["Pass/Fail (1=Pass, 0=Fail)"]
+X_test, y_test = test_df.drop("Pass/Fail (1=Pass, 0=Fail)", axis=1), test_df["Pass/Fail (1=Pass, 0=Fail)"]
 
-# log start time
-total_start = time.time()
+pipeline.fit(X_train, y_train)
 
-for model_name, model in tqdm(models_dict.items()):
-    val_rec_scores = []
-    val_gmean_scores = []
-    val_accuracy_scores = []
+y_pred = pipeline.predict(X_test)
 
-    for train_index, val_index in skf.split(X_trainval, y_trainval):
-        X_train, X_val = X_trainval.iloc[train_index], X_trainval.iloc[val_index]
-        y_train, y_val = y_trainval.iloc[train_index], y_trainval.iloc[val_index]
-
-        start_time = time.time()  # for logging run times
-
-        # fit
-        model.fit(X_train, y_train)
-
-        # predict
-        val_preds = model.predict(X_holdout)
-
-        val_rec_score = sensitivity_score(y_holdout, val_preds)
-        val_gmean_score = geometric_mean_score(y_holdout, val_preds)
-        val_accuracy_score = accuracy_score(y_holdout, val_preds)
-
-        end_time = time.time()  # for logging run times
-
-        val_rec_scores.append(val_rec_score)
-        val_gmean_scores.append(val_gmean_score)
-        val_accuracy_scores.append(val_accuracy_score)
-
-    res[model_name] = {
-        "ave_val_recall": np.mean(val_rec_scores) * 100,
-        "ave_val_gmean_score": np.mean(val_gmean_scores) * 100,
-        "ave_val_accuracy_score": np.mean(val_accuracy_scores) * 100,
-        "run_time": end_time - start_time,
-    }
-
-# log end time
-total_end = time.time()
-
-elapsed = total_end - total_start
-# print(f"Report Generated in {elapsed:.2f} seconds")
-res = pd.DataFrame(res).T
-print(res)
+print(f"Test Accuracy: {accuracy_score(y_test, y_pred) * 100:.2f}")
+print(f"Test Recall: {sensitivity_score(y_test, y_pred) * 100:.2f}")
+print(f"Test G-Mean: {geometric_mean_score(y_test, y_pred) * 100:.2f}")
