c4e1b57b6ca62e02a135b8f27f57d665802c2bc9
Cutiepatotieee <ced.encarnacion@gmail.com>
2025-08-20 04:09:53 +0800

    added data_ingestion.py to load data

---
+++ src/data_ingestion.py
@@ -0,0 +1,9 @@
+import gdown
+
+
+def load():
+    file_id = "16_IoRl6EUCevWf4_l5orzRLjlnVG-WUd"
+    url = f"https://drive.google.com/uc?id={file_id}"
+    output = "data/bronze/ml2_student_performance.csv"
+
+    gdown.download(url, output, quiet=False)
