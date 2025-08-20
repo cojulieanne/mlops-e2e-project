c93913c5c7dc2cb75e5beee4737ef5c6525b6be2
Cutiepatotieee <ced.encarnacion@gmail.com>
2025-08-20 18:23:39 +0800

    adjusted run_pipeline.py to meet specs required for the project

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -1,15 +1,17 @@
+import mlflow
 from load_data import load_data
 from preprocessing import preprocess_data
-from feature_engineering import split_data
 from training import training
 from evaluation import evaluation
 from utils import logger
+from drift_detection import detect_drift   # 👈 import drift detection
 
 logger = logger.get_logger(__name__)
 
 
 def main():
     logger.info("Starting the ML Pipeline...")
+
     logger.info("Loading Data...")
     load_data.load()
     logger.info("Loading Data Completed.")
@@ -18,10 +20,6 @@ def main():
     X_train, X_test, y_train, y_test, X_train_drifted, X_test_drifted = preprocess_data.preprocess_data()
     logger.info("Preprocessing completed.")
 
-    # logger.info("Splitting Data...")
-    # train_data, test_data = split_data.split_data(preprocessed_data)
-    # logger.info("Data Splitting Completed.")
-
     logger.info("Training Model...")
     model = training.training(X_train, y_train)
     logger.info("Model Training Completed.")
@@ -29,6 +27,28 @@ def main():
     logger.info("Evaluating Model...")
     evaluation.evaluation(X_train, y_train, X_test, y_test)
     logger.info("Model Evaluation Completed.")
+
+    logger.info("Running Drift Detection...")
+
+    test_drift_results = detect_drift(
+        "data/silver/preprocessed_ml2_student_performance.csv",
+        "data/silver/drifted_test.csv" 
+    )
+    mlflow.log_param("test_drift_detected", test_drift_results["drift_detected"])
+    mlflow.log_param("test_overall_drift_score", test_drift_results["overall_drift_score"])
+
+    if test_drift_results["drift_detected"]:
+        raise ValueError("Data drift detected in test set! Model retraining required.")
+
+    train_drift_results = detect_drift(
+        "data/silver/preprocessed_ml2_student_performance.csv", 
+        "data/silver/drifted_train.csv" 
+    )
+    mlflow.log_param("train_drift_detected", train_drift_results["drift_detected"])
+    mlflow.log_param("train_overall_drift_score", train_drift_results["overall_drift_score"])
+
+    if train_drift_results["drift_detected"]:
+        raise ValueError("Data drift detected in training set! Model retraining required.")
 
     logger.info("Pipeline completed successfully.")
 
